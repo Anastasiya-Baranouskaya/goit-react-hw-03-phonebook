31f00a7a8b835e155b7c7f6b52f7e091ece2bfac
Anastasiya-Baranouskaya <nastenkabv@gmail.com>
2021-12-23 15:35:16 +0100

    fi err

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -24,9 +24,6 @@ ContactList.propTypes = {
   list: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
     }),
   ),
-  onDeleteContact: PropTypes.func.isRequired,
 };
